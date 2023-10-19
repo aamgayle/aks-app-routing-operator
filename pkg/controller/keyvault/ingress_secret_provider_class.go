8d00bba45b5344ed19464f89cb2a50dac909b1c6
asgayle <asgayle@microsoft.com>
2023-10-19 14:49:45 -0400

    removed label check from spc

--- pkg/controller/keyvault/ingress_secret_provider_class.go
+++ pkg/controller/keyvault/ingress_secret_provider_class.go
@@ -92,7 +92,7 @@ func (i *IngressSecretProviderClassReconciler) Reconcile(ctx context.Context, re
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      fmt.Sprintf("keyvault-%s", ing.Name),
 			Namespace: ing.Namespace,
-			Labels:    ing.Labels,
+			Labels:    manifests.GetTopLevelLabels(),
 			OwnerReferences: []metav1.OwnerReference{{
 				APIVersion: ing.APIVersion,
 				Controller: util.BoolPtr(true),
@@ -133,10 +133,6 @@ func (i *IngressSecretProviderClassReconciler) Reconcile(ctx context.Context, re
 
 func (i *IngressSecretProviderClassReconciler) buildSPC(ing *netv1.Ingress, spc *secv1.SecretProviderClass) (bool, error) {
 	if ing.Spec.IngressClassName == nil || ing.Annotations == nil {
-		return false, nil
-	}
-
-	if len(ing.Labels) == 0 || !(manifests.HasRequiredLabels(ing.Labels, manifests.GetTopLevelLabels())) {
 		return false, nil
 	}
 
